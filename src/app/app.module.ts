5c1cb9915e5810f9aa95a3f516be12f438b7c029
Diannellys Marcano <diannellys@gmail.com>
2019-03-25 18:18:46 -0300

    Modificacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//rutas
 import { APP_ROUTES} from './app.routes';
+
+//modulos
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register/register.component';
+import { PagesModule } from './pages/pages.module';
 
 
 @NgModule({
@@ -20,18 +21,20 @@ import { RegisterComponent } from './login/register/register.component';
     AppComponent,
     RegisterComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Grafica1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    PagesComponent
+    // PagesComponent
+    // DashboardComponent,
+    // ProgressComponent,
+    // Grafica1Component,
+    //NopagefoundComponent,
+    // HeaderComponent,
+    // SidebarComponent,
+    // BreadcrumbsComponent,
+
   ],
   imports: [
     BrowserModule,
-    APP_ROUTES
+    APP_ROUTES,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
